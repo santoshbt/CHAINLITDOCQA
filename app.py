6bb081d9e256b69cb0c0f20dd50f26b4e17fa660
llssbb <bt.santosh01@gmail.com>
2024-05-05 17:38:52 +0100

    Updated files for initial setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains import (
+    ConversationalRetrievalChain,
+)
+from langchain.chat_models import ChatOpenAI
+
+from langchain.docstore.document import Document
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+
+import chainlit as cl
